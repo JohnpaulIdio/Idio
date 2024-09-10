179cb87df1270f4b1bc68ec01828cf3fdd35a173
C24torres <charlietorres09566701327@gmail.com>
2024-09-09 19:59:20 -0700

    data gathering

--- idio/app/Header.jsx
+++ idio/app/Header.jsx
@@ -25,7 +25,16 @@ import { data } from './data';
 const Header = () => {
   return (
     <div> {data.map((data, key) => (
-                <li key={key}>{data.name}</li>
+      <ol>
+        <li key={key}>{data.name}</li>
+        <li key={key}>{data.profession}</li>
+        <li key={key}>{data.age}</li>
+        <li key={key}>{data.email}</li>
+        <li key={key}>{data.username}</li>
+        <li key={key}>{data.city}</li>
+        <li key={key}>{data.address}</li>
+      </ol>
+                
         
              )) }</div>
   )
--- idio/app/data.js
+++ idio/app/data.js
@@ -4,32 +4,40 @@ export const data = [
         name: "John Doe",
     profession: "Full stack dev",
     age: 25,
+    email: "johndoe@email.com"
+    username: "johndoe03"
+
     },
     {
     id: 2,
-    name:"Manny",
+        name: "Manny",
     proffesion: "Full stack dev",
     age: 25,
+    email: "manny03@gmail.com"
     },
 {
     id: 3,
-    name: "Sarah duterte",
+        name: "Sarah duterte",
     proffesion: "Full stack dev",
     age: 25,
+    email: "sarahduter69@gmail.com"
 },
 
 {
     id: 4,
-    name: "Johnpaul",
+        name: "Johnpaul idio",
     proffesion: "Full stack dev",
     age: 25,
+    email: "idiojohnpaul011@gmail.com"
 
 },
 {
     id: 5,
-    name: "John rick",
+        name: "John rick",
     proffesion: "Full stack dev",
     age: 25,
+    email: "johnrick011@gmail.com"
+    
     
 },
 ]
